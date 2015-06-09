c7fbe7d302bb4794a4a7a424869a0324142f70d8
Samael500 <samael500@gmail.com>
2015-06-09 14:07:33 +0300

    :rocket: init django proj

---
+++ insta/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns(
+    '',
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
