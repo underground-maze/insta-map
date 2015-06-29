ee82b615cf27fb9cfb6a4d8a0e3d78d29a7150cb
Samael500 <samael500@gmail.com>
2015-06-29 12:09:00 +0300

    :repeat: add static script files

--- insta/index/static/maps/maps.js
+++ insta/index/static/maps/maps.js
@@ -1,32 +1,39 @@
-function initialize() {
-    // Create the map.
-    var mapOptions = {zoom: 10, center: new google.maps.LatLng(44.654675, 33.771838)};
+$(document).ready(function () {
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    function init_map() {
+        // Create the map.
+        var mapOptions = {zoom: 10, center: new google.maps.LatLng(44.654675, 33.771838)};
 
-    var fog_of_war = {
-        strokeColor: '#AAAAAA', strokeOpacity: 0.3, strokeWeight: 15,
-        fillColor: '#EEEEEE', fillOpacity: 0.9,
-        map: map,
-        paths: polygons
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+        var fog_of_war = {
+            strokeColor: '#AAAAAA', strokeOpacity: 0.3, strokeWeight: 15,
+            fillColor: '#EEEEEE', fillOpacity: 0.9,
+            map: map,
+            paths: polygons
+        };
+        cityCircle = new google.maps.Polygon(fog_of_war);
+
+        // add markers events
+        markers.forEach(
+            function(marker){
+                google.maps.event.addListener(marker, 'click', function() {
+                    open_modal(marker);
+                });
+            }
+        );
+
+        var cluster = new MarkerClusterer(map, markers);
     };
-    cityCircle = new google.maps.Polygon(fog_of_war);
 
-    // add markers events
-    markers.forEach(
-        function(marker){
-            google.maps.event.addListener(marker, 'click', function() {
-                var marker_info =
-                    'position: ' + marker.position + '\n' +
-                    'description: ' + marker.description + '\n' +
-                    'video: ' + marker.video;
-                alert(marker_info);
-            });
-        }
-    );
+    function open_modal(marker){
+        var $modal = $('.modal#card-info'),
+            label = 'Я первооткрыватель {coord}';
+        $modal.find('iframe').attr('src', marker.video);
+        $modal.find('div[name="description"]').text(marker.description);
+        $modal.find('#card-info-label').text(label.replace('{coord}', marker.position));
+        $('#open-modal').click();
+    };
 
-    var cluster = new MarkerClusterer(map, markers);
-
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+    init_map();
+});
