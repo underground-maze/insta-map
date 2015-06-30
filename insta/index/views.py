b14a170f84e83d7ea8a60f0facfa5613766b367f
Samael500 <samael500@gmail.com>
2015-06-30 12:31:19 +0300

    :repeat: site update view

--- insta/index/views.py
+++ insta/index/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View, TemplateView
 
 
 class IndexView(TemplateView):
@@ -7,6 +9,27 @@ class IndexView(TemplateView):
 
     http_method_names = ('get', )
     template_name = 'index.html'
+
+
+class SiteUpdate(View):
+
+    """ Update site view """
+
+    http_method_names = ('post', )
+    template_name = 'index.html'
+
+    @csrf_exempt
+    @login_required
+    @render_to_json
+    def dispatch(self, request, *args, **kwargs):
+        if not request.is_ajax():
+            return dict(result='errors')
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        """ Process post for update site """
+        form = self.get_form()
+        return dict(result='success')
 
 
 index_view = IndexView.as_view()
