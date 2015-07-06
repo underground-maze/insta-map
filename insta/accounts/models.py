2280b034c24389ca2cea5b0aefbe9630d6ef78b2
Samael500 <samael500@gmail.com>
2015-07-06 14:42:42 +0300

    :rocket: add insta user models

---
+++ insta/accounts/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class InstaUser(AbstractUser):
+
+    """ Custom user model for additional fields """
+
+    USERNAME_FIELD = 'username'
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+    def save(self, *args, **kwargs):
+        """ Set username equal to email """
+        if not self.username:
+            self.username = self.email
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+        db_table = 'auth_user'
