13a840d0d3c5ff80b55647e1716aea7e7071b8ec
Samael500 <samael500@gmail.com>
2015-07-15 18:31:53 +0300

    :repeat: no use subfolders

--- insta/helpers/service.py
+++ insta/helpers/service.py
@@ -10,11 +10,11 @@ from django.template.loader import render_to_string
 def video_path(instance, filename, directory='video'):
     """
     Path to video
-    *directory*/ab/cd/abcdef0123456789abcdef0123456789.mov
+    *directory*/abcdef0123456789abcdef0123456789.mov
     """
     basename, ext = os.path.splitext(filename.lower())
     hashed_name = hashlib.md5('{}{}{}'.format(uuid.uuid4(), filename, timezone.now()).encode('utf-8')).hexdigest()
-    return os.path.join(directory, hashed_name[:2], hashed_name[2:4], hashed_name + ext)
+    return os.path.join(directory, hashed_name + ext)
 
 
 def write_js(polygons, markers, cards_data):
