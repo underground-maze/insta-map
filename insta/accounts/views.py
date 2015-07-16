2fc05fae517b21024e387b7d08047180a0ba2382
Samael500 <samael500@gmail.com>
2015-07-16 21:06:34 +0300

    :necktie: pep8 fix

--- insta/accounts/views.py
+++ insta/accounts/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import logout
 def login_view(request):
     return redirect('/login/vk-oauth2')
 
+
 def logout_view(request):
     logout(request)
     return redirect('/')
