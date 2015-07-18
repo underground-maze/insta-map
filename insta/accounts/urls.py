cc621290b61e7301e8020c29cbbca993167ed18e
Samael500 <samael500@gmail.com>
2015-07-18 12:50:56 +0300

    :repeat: allow traling slash

--- insta/accounts/urls.py
+++ insta/accounts/urls.py
@@ -3,6 +3,6 @@ from accounts import views
 
 
 urlpatterns = (
-    url(r'^login$', views.login_view, name='login'),
-    url(r'^logout$', views.logout_view, name='logout'),
+    url(r'^login', views.login_view, name='login'),
+    url(r'^logout', views.logout_view, name='logout'),
 )
