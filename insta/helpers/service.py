c8fef39a7bdaa6f80b8c35788aecc68144013210
Samael500 <samael500@gmail.com>
2015-07-20 14:18:48 +0300

    :rocket: update site as helpers function

--- insta/helpers/service.py
+++ insta/helpers/service.py
@@ -1,3 +1,4 @@
+import json
 import hashlib
 import os
 import uuid
@@ -5,6 +6,8 @@ import uuid
 from django.conf import settings
 from django.utils import timezone
 from django.template.loader import render_to_string
+
+from helpers.coordinates import get_map_polygons, get_map_markers
 
 
 def video_path(instance, filename, directory='video'):
@@ -29,11 +32,23 @@ def write_js(polygons, markers, cards_data):
         js.write(content)
 
 
-def render_to_file(template_name, context, file_name):
-    """
-    Render html to file put file in BASE_DIR/markup/*file_name*
-    """
+def render_to_file(template_name, context=None, file_name=None):
+    """ Render html to file put file in BASE_DIR/markup/*file_name* """
+    context = context or dict()
+    file_name = file_name or template_name
     context.update(dict(STATIC_URL=settings.STATIC_URL))
     file_path = os.path.join(settings.BASE_DIR, 'markup', file_name)
     with open(file_path, 'w', encoding='UTF-8') as output:
         output.write(render_to_string(template_name, context))
+
+
+def site_update(active, template_name):
+    """ Recalculate all coordinates and place new files """
+    # get js representation of card polygons
+    polygons = get_map_polygons([card.as_tuple() for card in active])
+    markers = get_map_markers([card.as_dict() for card in active])
+    markers_data = json.dumps({card.pk: card.as_dict() for card in active})
+    # write the script into file
+    write_js(polygons, markers, markers_data)
+    # update index.html
+    render_to_file(template_name)
