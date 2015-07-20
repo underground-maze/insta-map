75214f067ad51535e3e9c7b7a9129b93ee84fe75
Samael500 <samael500@gmail.com>
2015-07-20 16:55:16 +0300

    :wrench: correct geolocation

--- insta/index/static/maps/maps.js
+++ insta/index/static/maps/maps.js
@@ -130,7 +130,7 @@ $(document).ready(function () {
 
     function get_position(position){
         init_mini_map(position.coords.latitude, position.coords.longitude, 13);
-        var coords = '(' + position.coords.latitude + ',' + position.coords.longitude + ')';
+        var coords = '(' + position.coords.latitude + ', ' + position.coords.longitude + ')';
         $('#add-card').find('input[name="position"]').val(coords)
     }
 
