8b8b8c4ac904691e799425b343d13f0599322011
Samael500 <samael500@gmail.com>
2015-07-20 12:41:32 +0300

    :repeat: users in auth app

--- insta/accounts/models.py
+++ insta/accounts/models.py
@@ -18,6 +18,7 @@ class InstaUser(AbstractUser):
         return super().save(*args, **kwargs)
 
     class Meta:
+        app_label = 'auth'
         verbose_name = 'пользователь'
         verbose_name_plural = 'пользователи'
         db_table = 'auth_user'
